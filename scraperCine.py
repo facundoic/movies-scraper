a33ef876da160b8e79d8e040cb435659a9f671c3
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-29 19:26:33 -0300

    Update scraperCine.py

--- scraperCine.py
+++ scraperCine.py
@@ -5,6 +5,7 @@ import sys
 page = requests.get("https://www.todoshowcase.com/")
 soup = BeautifulSoup(page.content, "html.parser")
 flag = False
+movieF = ""
 
 if len(sys.argv) == 2:
     print(soup.title.string+"\n")
@@ -19,11 +20,12 @@ movieName = str(sys.argv[1])
 movies = soup.find_all("div", {"class": "titulo-pelicula"})
 
 for movie in movies:
-    if movie.h2.a.text.strip() == movieName:
+    if movieName in movie.h2.a.text.strip():
         flag = True
+        movieF = movie.h2.a.text.strip()
         break
     
 if flag == True:
-    print("El cine tiene tu pelicula")
+    print(f"El cine tiene tu pelicula: {movieF}")
 else:
      print("Lo siento no pude encontrarla")
